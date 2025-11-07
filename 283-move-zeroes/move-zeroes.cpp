4e4b882213bcb406ecb915b0d63849c976106e69
Sajal Rajhans <sajalrajhans@gmail.com>
2025-11-07 22:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (55.05%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int last = 0; // index of next position for non-zero
-        for (int i = 0; i < (int)nums.size(); ++i) {
-            if (nums[i] != 0)
-                swap(nums[i], nums[last++]);
+        int j = -1;
+        for (int i = 0; i < (int)nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+        if (j == -1)
+            return; 
+
+        for (int i = j + 1; i < (int)nums.size(); i++) {
+            if (nums[i] != 0) {
+                std::swap(nums[j], nums[i]);
+                j++; 
+            }
         }
     }
 };
