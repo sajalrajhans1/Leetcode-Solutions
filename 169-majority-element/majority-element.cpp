260bf6161146391fd67d5fa030bc3bf37b848aa4
Sajal Rajhans <sajalrajhans@gmail.com>
2026-01-04 15:56:47 +0530

    Time: 1663 ms (5.00%) | Memory: 28.3 MB (27.54%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,17 +4,17 @@ public:
         int n = nums.size();
         int comp = n / 2;
 
-        for (int i = 0; i < n; i++) {
-            int count = 0;              
-            for (int j = 0; j < n; j++) { 
-                if (nums[i] == nums[j]) {
-                    count++;
+        for(int i=0; i<n; i++){
+            int cnt = 0;
+            for(int j=0; j<n; j++){
+                if(nums[i] == nums[j]){
+                    cnt++;
                 }
             }
-            if (count > comp) {
+            if(cnt > comp){
                 return nums[i];
             }
         }
-        return -1;
+        return -1; 
     }
 };
